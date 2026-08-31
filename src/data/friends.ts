9450584a15b30c8c58bf10cb9bee2fce53331dcf
DonSY1 <2486571990@qq.com>
2026-08-31 09:08:20 +0800

    12

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -12,14 +12,14 @@ export interface FriendItem {
 
 // 友情链接数据
 export const friendsData: FriendItem[] = [
-	// {
-	// 	id: 1,
-	// 	title: "Astro",
-	// 	imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-	// 	desc: "The web framework for content-driven websites",
-	// 	siteurl: "https://github.com/withastro/astro",
-	// 	tags: ["Framework"],
-	// },
+	{
+		id: 1,
+		title: "午安",
+		imgurl: "https://blog.711250.xyz/_astro/wuan.CBihK-Dp_Z2gIanU.webp",
+		desc: "喜欢睡午觉",
+		siteurl: "https://blog.711250.xyz/",
+		tags: ["Friend", "Blog"],
+	},
 	// {
 	// 	id: 2,
 	// 	title: "Mizuki Docs",
