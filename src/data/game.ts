3bb32202b47d957605c7687179e0826897c1719d
DonSY1 <2486571990@qq.com>
2026-09-11 14:12:29 +0800

    16.1

--- src/data/game.ts
+++ src/data/game.ts
@@ -20,6 +20,30 @@ export const gameData: GameItem[] = [
 		siteurl: "/games/game.html",
 		tags: ["Game"],
 	},
+	{
+		id: 2,
+		title: "贪吃蛇",
+		imgurl: "https://blog.yindiot.xyz/_astro/avatar.TikwgH7w_1A9ymT.webp",
+		desc: "控制蛇吃掉食物，避免撞墙和自己",
+		siteurl: "/games/snake.html",
+		tags: ["Game"],
+	},
+	{
+		id: 3,
+		title: "俄罗斯方块",
+		imgurl: "https://blog.yindiot.xyz/_astro/avatar.TikwgH7w_1A9ymT.webp",
+		desc: "消除行以获得高分",
+		siteurl: "/games/tetris.html",
+		tags: ["Game"],
+	},
+	{
+		id: 4,
+		title: "扫雷",
+		imgurl: "https://blog.yindiot.xyz/_astro/avatar.TikwgH7w_1A9ymT.webp",
+		desc: "在雷区中找到所有安全的方块",
+		siteurl: "/games/mine_clearance.html",
+		tags: ["Game"],
+	},
 ];
 
 // 获取所有游戏数据
